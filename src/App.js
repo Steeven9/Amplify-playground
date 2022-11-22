cd7f207a63a496426dcd3816c71ec3eb6f66dd1e
Stefano Taillefert <stefano.taille@gmail.com>
2022-11-22 22:53:19 +0000

    Improve images and fetching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import axios from "axios";
 import React, { useState } from "react";
+import loading from "./img/loading.gif";
+import notFound from "./img/notFound.webp";
 
 function App() {
-  const [text, setText] = useState("");
   const [img, setImg] = useState("");
   const [shinyImg, setShinyImg] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -10,35 +10,58 @@ function App() {
   const [hasSearched, setHasSearched] = useState(false);
 
   const serebiiURL = "https://www.serebii.net/scarletviolet/pokemon";
+  // const serebiiTextualURL = "https://www.serebii.net/pokemon/art";
   const serebiiShinyURL = "https://www.serebii.net/Shiny/SV";
-  const notFoundURL =
-    "https://media.giphy.com/media/lkdH8FmImcGoylv3t3/giphy.gif";
-  const loadingURL = "https://media.tenor.com/RVvnVPK-6dcAAAAM/reload-cat.gif";
 
   const fetchData = async () => {
     setHasSearched(true);
-    try {
-      setIsLoading(true);
-      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${text}`);
-      console.info(res.data);
-      let id = res.data.id;
-      if (id < 100) {
+    const queryText = document.querySelector("#searchbar").value;
+    let dex;
+    document.querySelectorAll(".dexRadio").forEach((el) => {
+      if (el.checked) {
+        dex = el.value;
+      }
+    });
+    let id;
+
+    if (isNaN(queryText)) {
+      // name
+      //TODO request to pokeAPI
+
+      alert("Please use the number (for now)");
+    } else {
+      // id
+      id = Number(queryText);
+      if (dex === "SV") {
+        id += 905;
+      }
+      if (id < 10) {
+        id = `00${id}`;
+      } else if (id < 100) {
         id = `0${id}`;
       }
+      console.log(id);
+      console.log(dex);
+    }
 
-      setImg(`${serebiiURL}/${id}.png`);
-      setShinyImg(`${serebiiShinyURL}/${id}.png`);
-      setIsFound(true);
+    try {
+      setIsLoading(true);
+      let image = new Image();
+
+      image.onload = function () {
+        setImg(`${serebiiURL}/${id}.png`);
+        setShinyImg(`${serebiiShinyURL}/${id}.png`);
+        setIsFound(true);
+      };
+      image.onerror = function () {
+        console.error("Not found");
+        setIsFound(false);
+      };
+
+      image.src = `${serebiiURL}/${id}.png`;
     } catch (error) {
       console.error(error);
-
-      if (!isNaN(text)) {
-        setImg(`${serebiiURL}/${text}.png`);
-        setShinyImg(`${serebiiShinyURL}/${text}.png`);
-        setIsFound(true);
-      } else {
-        setIsFound(false);
-      }
+      setIsFound(false);
     } finally {
       setIsLoading(false);
     }
@@ -53,33 +76,53 @@ function App() {
   return (
     <>
       <h3>Ez shiny Pokémon finder 9000</h3>
-      If it says not found use the number plz k thx. If still not found, add 905
-      to the number. Shhh don't ask
+      If it says not found use the number plz k thx.
       <br />
+      <label>
+        <input
+          type="radio"
+          value="SV"
+          name="dex"
+          className="dexRadio"
+          checked
+        />{" "}
+        SV regional
+      </label>
+      <label>
+        <input type="radio" value="SwSh" name="dex" className="dexRadio" />{" "}
+        Global
+      </label>
       <input
         type="text"
-        placeholder="Pokémon name or #"
-        onKeyDown={(event) => setText(event.target.value)}
+        id="searchbar"
+        placeholder="Pokémon name or number"
         onKeyUp={handleInput}
-        style={{ margin: "20px" }}
+        style={{
+          margin: "20px",
+          outline: "none",
+          width: "400px",
+          padding: "10px",
+        }}
       />
       <button onClick={fetchData}>Search</button>
       <br />
       {isLoading ? (
-        <img src={loadingURL} alt="Loading" />
+        <img src={loading} alt="Loading" />
       ) : isFound ? (
         <>
           <div>
-            <img src={img} alt="" style={{ verticalAlign: "middle" }} />
             Normal sprite
+            <br />
+            <img src={img} alt="" style={{ verticalAlign: "middle" }} />
           </div>
           <div>
-            <img src={shinyImg} alt="" style={{ verticalAlign: "middle" }} />
             Shiny sprite
+            <br />
+            <img src={shinyImg} alt="" style={{ verticalAlign: "middle" }} />
           </div>
         </>
       ) : hasSearched ? (
-        <img src={notFoundURL} alt="Not found" />
+        <img src={notFound} alt="Not found" />
       ) : null}
     </>
   );
